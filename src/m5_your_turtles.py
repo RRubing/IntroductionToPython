d9d836423ad646f6436363aba3e4a169c65a748a
wur <wur@rose-hulman.edu>
2020-06-04 16:37:38 -0400

    10

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Rubing Wu.
 """
 ########################################################################
-# TODO: 1.
+# Done: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# Done: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,38 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+Teemo = rg.SimpleTurtle('turtle')
+Teemo.pen = rg.Pen('Orange', 5)
+Teemo.speed = 1
+
+Yasuo = rg.SimpleTurtle('turtle')
+Yasuo.pen = rg.Pen('blue', 5)
+Yasuo.speed = 2
+
+size = 150
+
+for k in range (10):
+
+    Teemo.draw_square(size)
+    Yasuo.draw_square(size)
+
+    Teemo.pen_up()
+    Teemo.left(10)  # degree angle
+    Teemo.forward(45)
+    Teemo.right(45)  # degree angle
+    Teemo.pen_down()
+
+    Yasuo.pen_up()
+    Yasuo.left(10)
+    Yasuo.forward(45)
+    Yasuo.right(45)
+    Yasuo.pen_down()
+
+    size = size - 12
+
+window.close_on_mouse_click()
